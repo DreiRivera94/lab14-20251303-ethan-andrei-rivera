7e0c45adc89781395270f9fe997834f56a56f24d
Ethan Andrei Rivera <ethanrivera0409@gmail.com>
2026-06-03 16:13:47 +1200

    Added anime watchlist improvement

--- anime_watchlist.py
+++ anime_watchlist.py
@@ -1,13 +1,15 @@
 class Anime:
-  def __init__(self, title, genre, status):
+  def __init__(self, title, genre, status, rating):
     self.title = title
     self.genre = genre
     self.status = status
+    self.rating = rating
 
   def displayInfo(self):
     print("Title:", self.title)
     print("Genre:", self.genre)
     print("Status:", self.status)
+    print("Rating:", self.rating)
 
   def updateStatus(self, newstatus):
     self.status = newstatus
@@ -15,14 +17,15 @@ class Anime:
     print("Title:", self.title)
     print("Genre:", self.genre)
     print("Status:", self.status)
+    print("Rating:", self.rating)
 
-anime1 = Anime("Steins;Gate", "Drama, Sci-Fi", "Completed")
+anime1 = Anime("Steins;Gate", "Drama, Sci-Fi", "Completed", "10")
 anime1.displayInfo()
 
-anime2 = Anime("Kaguya-sama: Love is War -Ultra Romantic-", "Comedy, Romance", "Completed")
+anime2 = Anime("Kaguya-sama: Love is War -Ultra Romantic-", "Comedy, Romance", "Completed", "9")
 anime2.displayInfo()
 
-anime3 = Anime("Fullmetal Alchemist: Brotherhood", "Action, Drama", "Plan to Watch")
+anime3 = Anime("Fullmetal Alchemist: Brotherhood", "Action, Drama", "Plan to Watch", "9")
 anime3.displayInfo()
 
 anime3.updateStatus("Watching")
